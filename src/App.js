bfc5439a4b35b6f038f92c3695dd70cd54a76e21
gaabsoares <gabrielsoaresdelima@hotmail.com>
2023-12-25 21:24:05 -0300

    app accetps props

--- src/App.js
+++ src/App.js
@@ -4,6 +4,20 @@ import './App.css';
 import BusinessList from './components/BusinessList';
 import SearchBar  from './components/SearchBar';
 
+export const businessExample = {
+    imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+    name: 'MarginOtto Pizzeria',
+    address: '1010 Paddington Way',
+    city: 'Flavortown',
+    state: 'NY',
+    zipCode: '10101',
+    category: 'Italian',
+    rating: 4.5,
+    reviewCount: 90
+};
+
+export let businessList = [businessExample, businessExample, businessExample, businessExample, businessExample, businessExample];
+
 function App() {
   return (
     <div className="App">
--- src/components/BusinessList.js
+++ src/components/BusinessList.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import {businessExample} from './Business';
+import { businessExample } from '../App';
 import './BusinessList.css';
 
 import Business from './Business';
-
-let businessList = [businessExample, businessExample, businessExample, businessExample, businessExample, businessExample]
+import { businessList } from '../App';
 
 function BusinessList(props) {
     return (
         <div className="business-list">
-            {businessList.map(business => <Business />)}
+            {businessList.map(business => <Business 
+            imageSrc={business.imageSrc} 
+            name={business.name} 
+            address={business.address} 
+            city={business.city} 
+            state={business.state} 
+            zipCode={business.zipCode} 
+            category={business.category} 
+            rating={business.rating} 
+            reviewCount={business.reviewCount} />)}
         </div>
     );
 };
