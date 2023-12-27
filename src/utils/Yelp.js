e74133237380b40375e9fdf9624e9c73714bb9da
gaabsoares <gabrielsoaresdelima@hotmail.com>
2023-12-27 14:33:27 -0300

    finish app / working

---
+++ src/utils/Yelp.js
@@ -0,0 +1,30 @@
+const apiKey = 'AzNhV29wCNFwd2VMZykdAcghyPBvXM0yUn-ASCgURhn3LcjoxotpTC9xpjMngSPk-ah2b2mQ0OL3Z4JzzOccybMmN47HY3H207DNUPuGyk-ONJoPdBf-m5JTUjyMZXYx';
+
+const Yelp = {
+    async search(term, loc, sort) {
+        return fetch(`https://cors-anywhere.herokuapp.com/api.yelp.com/v3/businesses/search?term=${term}&location=${loc}&sort_by=${sort}`,
+            {
+                headers: { Authorization: `Bearer ${apiKey}` },
+            }
+        ).then((response) => {
+            return response.json();
+        }).then((jsonResponse) => {
+            if(jsonResponse.businesses) {
+                return jsonResponse.businesses.map((business) => ({
+                    id: business.id,
+                    imageSrc: business.image_url,
+                    name: business.name,
+                    address: business.location.address1,
+                    city: business.location.city,
+                    state: business.location.state,
+                    zipCode: business.location.zip_code,
+                    category: business.categories[0].title,
+                    rating: business.rating,
+                    reviewCount: business.review_count,
+                }));
+            }
+        });
+    }
+};
+
+export default Yelp;
